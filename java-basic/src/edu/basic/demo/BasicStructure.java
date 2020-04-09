37a1bb0f804a4a9b6ea4cc1ca2d670276f66c17d
Tech_Hommie <pushp.204bhardwaj@gmail.com>
2020-04-09 21:12:50 +0530

    java naming conventions

---
+++ java-basic/src/edu/basic/demo/BasicStructure.java
@@ -0,0 +1,18 @@
+/* package name 
+ * prefix always written in lower case
+ * domain names like com, edu, gov, mil, net, org
+ * always it should be first line*/
+package edu.basic.demo;
+
+public class BasicStructure {
+/* 
+ * class name mixed with the first letter of each internal word capitalized
+  */
+	
+	//main method called by JVM
+	
+	public static void main(String[] args) {
+		System.out.println("Basic Structure of java");
+	}
+	
+}

d86f00d29b173fa55f2d9effb121b733a7fd1294
Tech_Hommie <pushp.204bhardwaj@gmail.com>
2020-04-09 22:24:51 +0530

    comment declaration

--- java-basic/src/edu/basic/demo/BasicInterfaceStructure.java
+++ java-basic/src/edu/basic/demo/BasicInterfaceStructure.java
@@ -1,6 +1,10 @@
 package edu.basic.demo;
 
 public interface BasicInterfaceStructure {
+	/*
+	 * interface name mixed with the first letter of each internal word capitalized
+	 */
 
+//	method declaration
 	void displayDetails(int id);
 }
